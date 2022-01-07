8b361d486e1fc2674d69905b00f68c4240fcd34e
udhaytenacia <udhay@tenaciatech.com>
2022-01-07 09:41:37 +0530

    home page added

---
+++ src/pages/hometemplate.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HomeTemplate extends Component {
+render(){
+    return(
+        <div className="container">
+            Home Page Template should be here.
+        </div>
+    )
+}
+
+}
+
+export default HomeTemplate;
